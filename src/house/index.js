21b03dea77048646319c44681b994b4aabe5b612
Simona <simonaeneva@yahoo.com>
2020-02-12 14:58:22 +0200

    Added Inquiry component and base functionality

--- src/house/index.js
+++ src/house/index.js
@@ -1,27 +1,43 @@
 import React, {Component} from "react";
 import "./house.css";
+import Inquiry from "./inquiry";
+
 
 class House extends Component {
-    state = {}
+    state = {
+        showInquiry: false
+    }
+
+    toggleInquiry = () => {
+        this.setState({showInquiry: !this.state.showInquiry});
+    }
 
     render() {
         const house = this.props.house;
-        return(
-            <div>
+        const inquiry = this.state.showInquiry ?
+            <Inquiry house={house}/> : null;
+
+        return (
+            <div className='wrapper'>
                 <div className='row mt-2'>
-                    <h5 className='col-md-12'>{house.country}</h5>
+                    <h5 className='col-md-12 country'>{house.country}</h5>
                 </div>
                 <div className='row'>
                     <h5 className='col-md-12'>{house.address}</h5>
                 </div>
                 <div className='row'>
                     <div className='col-md-7'>
-                        <img src={`https://images.pexels.com/photos/${house.photo}/pexels-photo-${house.photo}.jpeg?w=600&h=400&auto=compress&cs=tinysrgb`} alt="House"/>
+                            <img className='picture'
+                                 src={`https://images.pexels.com/photos/${house.photo}/pexels-photo-${house.photo}.jpeg?w=600&h=400&auto=compress&cs=tinysrgb`}
+                                 alt="House"/>
                     </div>
-                <div className='col-md-5'>
-                    <p className='price'>${house.price}</p>
-                    <p>{house.description}</p>
-                </div>
+                    <div className='col-md-5 property-info'>
+                        <p className='price'>${house.price}</p>
+                        <p>{house.description}</p>
+                        <button className="btn btn-block btn-outline-dark" onClick={this.toggleInquiry}>
+                            <i className="fa fa-envelope" ></i> Inquiries</button>
+                        {inquiry}
+                    </div>
                 </div>
             </div>
         );
